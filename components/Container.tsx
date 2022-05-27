7bfd0f4946605d35bb0d114475c0cccafe201bf3
José Aular <jose14aular@gmail>
2022-05-27 15:15:10 -0400

    Change meta url

--- components/Container.tsx
+++ components/Container.tsx
@@ -32,11 +32,11 @@ const Container = (props: Props) => {
         <meta content={meta.description} name="description" />
         <meta
           property="og:url"
-          content={`https://next-datocms-preview.vercel.app${meta.currentPage}`}
+          content={`https://next-mantineui.vercel.app${meta.currentPage}`}
         />
         <link
           rel="canonical"
-          href={`https://next-datocms-preview.vercel.app${meta.currentPage}`}
+          href={`https://next-mantineui.vercel.app${meta.currentPage}`}
         />
         <meta property="og:type" content={meta.type} />
         <meta property="og:site_name" content="Next" />
